834151259f5169a8b9758722425973481e48f4c2
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-19 15:54:47 +0630

    new class

--- hotel/models/models.py
+++ hotel/models/models.py
@@ -16,3 +16,26 @@ class hotel(models.Model):
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+class property(models.Model):
+    _name = 'property.property'
+    _description = 'propert property'
+
+    property_code = fields.Char()
+    property_name = fields.Char()
+    address1 = fields.Char()
+    address2 = fields.Char()
+    address3 = fields.Char()
+    country_id = fields.Char()
+    state_id = fields.char()
+    city_id = fields.Char()
+    telephone = fields.Char()
+    fax = fields.Char()
+    email = fields.Char()
+    website = fields.Char()
+    social_media_links = fields.Char()
+    no_of_room = fields.Integer(size=4)
+    proprety_license = fields.Char()
+    property_rating = fields.Char()
+    property_logo = fields.binary()
+    property_photo = fields.binary()
