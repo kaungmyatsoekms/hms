e9c57059a5390d6e09effcf96b7307e08a971bc2
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-20 12:03:19 +0630

    manifest

--- hotel/__manifest__.py
+++ hotel/__manifest__.py
@@ -27,7 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'view./hotelroom_view.xml'
+        'views/hotelroom_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
