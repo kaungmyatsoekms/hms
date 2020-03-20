b4a204b21eed9742fe1151f8fb8de0519785d6fc
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-20 10:53:49 +0630

    new class

---
+++ hotel/models/hotel_room.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class HotelRoomType(models.Model):
+    _name = 'hotel.roomtype'
+    _description = 'hotel.roomtype'
+
+    roomtype_code = fields.Char(string="Code", required=True)
+    roomtype_name = fields.Char(string="Name", required=True)
+    roomtype_rate = fields.Integer(string="Rate", required=True)
+    roomtype_bedqty = fields.Integer(string="Number of Bed", required=True)
+    roomtype_description = fields.Char(string="Description",required=True)
