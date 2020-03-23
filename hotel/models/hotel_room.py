abde6bba889483b2ebb2cdc77de2ba3e8ffe23c4
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-23 10:56:50 +0630

    new class

--- hotel/models/hotel_room.py
+++ hotel/models/hotel_room.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.osv import expression
 
 class HotelRoomType(models.Model):
     _name = 'hotel.roomtype'
@@ -29,3 +30,58 @@ class HotelRoom(models.Model):
     room_extension = fields.Char()
     room_image = fields.Binary()
     room_description = fields.Char()
+
+class HotelRoomAmenitiesType(models.Model):
+    _name = 'hotel.room.amenities.type'
+    _description = 'amenities type'
+
+    name = fields.Char('Name', size=64, required=True)
+    amenity_id = fields.Many2one('hotel.room.amenities.type','Category')
+    child_id = fields.One2many('hotel.room.amenities.type', 'amenity_id', 'Child Category')
+
+    @api.multi
+    def name_get(self):
+            def get_names(cat):
+
+                res = []
+                while cat:
+                    res.append(cat.name)
+                    cat = cat.amenity_id
+                    return res
+                return [(cat.id," / ".join(reversed(get_names(cat)))) for cat in self]
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        if not args:
+            args = []
+        if name:
+            # Be sure name_search is symetric to name_get
+            category_names = name.split(' / ')
+            parents = list(category_names)
+            child = parents.pop()
+            domain = [('name', operator, child)]
+            if parents:
+                names_ids = self.name_search( ' / '.join(parents), args=args,
+                                            operator='ilike', limit=limit)
+                category_ids = [name_id[o] for name_id in names_ids]
+
+                if operator in expression.NEGATIVE_TERM_OPERATORS:
+                    categories = self.search([('id', 'not in', category_ids)])
+                    domain = expression.OR([[('amenity_id', 'in', 
+                                                categories.ids)], domain])
+                else:
+                    domain = exprission.AND([[('categ_id', 'in',
+                                                 category_ids)], domain])
+                for i in range(1, len(category_names)):
+                    domain = [[('name', operator,
+                                ' / '.join(category_names[-1 - i:]))], domain]
+                    if operator in expression.NEGATIVE_TERM_OPERATORS:
+                        domain = expression.AND(domain)
+                    else:
+                        domain = expression.OR(domain)
+            categories = self.search(expression.AND([domain, args]),
+                                    limit=limit)
+        else:
+            categories = self.search(args, limit=limit)
+        return categories.name_get()            
+
