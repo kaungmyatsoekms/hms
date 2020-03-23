b7d0f014bd2244c3e83748d3f098413c91a855bc
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-23 16:34:51 +0630

    fix

--- hotel/models/hotel_room.py
+++ hotel/models/hotel_room.py
@@ -36,7 +36,7 @@ class HotelRoomView(models.Model):
     _description = 'hotel.roomview'
 
     roomview_code = fields.Char()
-    roomview_name = fields.One2many('hotel.room','roomview_id','View Name')
+    roomview_name = fields.Char()
     roomview_description = fields.Char()
 
 # class HotelRoomAmenitiesType(models.Model):
