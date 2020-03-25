9b2a66736f2c64d489b1eb55180f43cd2ba7d82e
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-25 08:45:59 +0630

    key error

--- hotel/models/models.py
+++ hotel/models/models.py
@@ -9,7 +9,7 @@ class Property(models.Model):
 
     name = fields.Char(string='Hotel Name', required=True)
     code = fields.Char(string='Hotel Code', required=True)
-    address1 = fields.Char(string='Address1', required=True)
+    address1 = fields.Char(string='Address', required=True)
     address2 = fields.Char(string='Address2', required=True)
     address3 = fields.Char(string='Address3', required=True)
     city_id = fields.Char(string='City')
@@ -22,7 +22,7 @@ class Property(models.Model):
     website = fields.Char(string='Website')
     sociallink = fields.Char(string='Social Media Link')
     no_of_room = fields.Integer(string='Total Rooms')
-    property_license = fields.Char(string='Property License')
+    property_license = fields.Char()
     property_rating = fields.Selection(
         [('one','*'),
          ('two','**'),
@@ -31,8 +31,14 @@ class Property(models.Model):
          ('five','*****'),
         ]
     )
+    note = fields.Text()
     property_logo = fields.Binary(string='Logo')
     property_image = fields.Binary(string='Image')
+    # is_group = fields.Boolean(string="Is a Group", default=False,
+    #     help="Check if the contact is a group, otherwise it is a hotel")
+    # user_type = fields.Selection(string='User Type',
+    #     selection=[('hotel','Hotel'),('group','Group')],
+    #     compute='-compute_user_type')inverse='_write_user_type'
 
 class HotelBuilding(models.Model):
     _name = 'hotel.building'
